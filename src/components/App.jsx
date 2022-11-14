1830b6c2b5551bc0b215073a6d592b7c2be19043
Anton <anton_surkov@rambler.ru>
2022-11-14 20:44:14 +0200

    fix

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -11,7 +11,7 @@ const App = () => {
   const [page, setPage] = useState(1);
   const [images, setImages] = useState([]);
 
-  const [setError] = useState('');
+  const [_, setError] = useState('');
   const [largeImage, setLargeImage] = useState(null);
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [isLoad, setIsLoad] = useState(false);
